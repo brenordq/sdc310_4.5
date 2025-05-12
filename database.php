5308392ef72d173cc0ff14bafabb390a940ccd3a
brenordq <b.nordq@gmail.com>
2025-05-12 18:20:42 -0400

    Update database.php

--- database.php
+++ database.php
@@ -5,7 +5,7 @@
         $hostname = "localhost";
         $username = "ecpi_user";
         $password = "Password1";
-        $dbname = "sdc310_wk3";
+        $dbname = "sdc310_wk4pa";
         return mysqli_connect($hostname, $username, $password, $dbname);
     }
 ?>
